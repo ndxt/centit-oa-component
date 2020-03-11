ef75bf394e28bdf957d8177b3dc81fe7c49c7bc8
zhf <zhf@centit.com>
2020-03-11 13:12:35 +0800

    添加id

--- oa-component-module/src/main/java/com/centit/product/oa/service/impl/InnerMessageManagerImpl.java
+++ oa-component-module/src/main/java/com/centit/product/oa/service/impl/InnerMessageManagerImpl.java
@@ -73,7 +73,8 @@ public class InnerMessageManagerImpl implements InnerMessageManager, MessageSend
                 if (bo > 0) {
                     receiveName += ",";
                 }
-                receiveName += CodeRepositoryUtil.getUserInfoByCode(userCode).getUserName();
+                IUserInfo userinfo=CodeRepositoryUtil.getUserInfoByCode(userCode);
+                receiveName += userinfo==null?userCode:userinfo.getUserName();
                 bo++;
             }
             msg.setReceiveName(receiveName);
@@ -180,14 +181,15 @@ public class InnerMessageManagerImpl implements InnerMessageManager, MessageSend
         msg.setMsgType("M");
         msg.setMailType("O");
         msg.setMsgState("U");
-        msg.setReceiveName(CodeRepositoryUtil.getUserInfoByCode(receiver).getUserName());
+        msg.setSender(sender);
+//        msg.setReceiveName(receiver);
         InnerMsgRecipient recipient = new InnerMsgRecipient();
         //recipient.setMInnerMsg(msg);
         recipient.setReplyMsgCode(0);
         //recipient.setReceiveType("P");
         recipient.setMailType("T");
         recipient.setMsgState("U");
-        String[] receives = new String[]{receiver};
+        String[] receives = receiver.split(",");
         sendToMany(receives, msg, recipient);
         return ResponseData.successResponse;
     }
