b3c3c70e6242c04b78d5c15b99b32f112a972c76
zhf <zhf@centit.com>
2020-03-16 17:49:10 +0800

    添加BBS接口

--- oa-component-module/src/main/java/com/centit/product/oa/service/BbsManager.java
+++ oa-component-module/src/main/java/com/centit/product/oa/service/BbsManager.java
@@ -1,4 +1,21 @@
 package com.centit.product.oa.service;
 
+import com.centit.product.oa.po.BbsPiece;
+import com.centit.product.oa.po.BbsSubject;
+import com.centit.product.oa.po.InnerMsg;
+import com.centit.support.database.utils.PageDesc;
+
+import java.util.List;
+import java.util.Map;
+
 public interface BbsManager {
+    List<BbsSubject> listBbsSubjects(Map<String, Object> filterMap, PageDesc pageDesc);
+    BbsSubject getBbsSubjectByID(String id);
+    void createBbsSubject(BbsSubject bbsSubject);
+    void updateBbsSubject(BbsSubject bbsSubject);
+    void deleteBbsSubjectByID(String id);
+    void createBbsPiece(BbsPiece bbsPiece);
+    void updateBbsPiece(BbsPiece bbsPiece);
+    void deleteBbsPieceByID(String id);
+
 }
--- oa-component-module/src/main/java/com/centit/product/oa/service/impl/BbsManagerImpl.java
+++ oa-component-module/src/main/java/com/centit/product/oa/service/impl/BbsManagerImpl.java
@@ -1,12 +1,63 @@
 package com.centit.product.oa.service.impl;
 
 import com.centit.product.oa.dao.BbsDao;
+import com.centit.product.oa.dao.BbsPieceDao;
+import com.centit.product.oa.po.BbsPiece;
+import com.centit.product.oa.po.BbsSubject;
 import com.centit.product.oa.service.BbsManager;
+import com.centit.support.database.utils.PageDesc;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class BbsManagerImpl implements BbsManager {
     @Autowired
     private BbsDao bbsDao;
+    @Autowired
+    private BbsPieceDao bbsPieceDao;
+
+    @Override
+    public List<BbsSubject> listBbsSubjects(Map<String, Object> filterMap, PageDesc pageDesc) {
+        return bbsDao.listObjects(filterMap,pageDesc);
+    }
+
+    @Override
+    public BbsSubject getBbsSubjectByID(String id) {
+        return bbsDao.getObjectById(id);
+    }
+
+    @Override
+    public void createBbsSubject(BbsSubject bbsSubject) {
+        bbsDao.saveNewObject(bbsSubject);
+    }
+
+    @Override
+    public void updateBbsSubject(BbsSubject bbsSubject) {
+        bbsDao.updateObject(bbsSubject);
+    }
+
+    @Override
+    public void deleteBbsSubjectByID(String id) {
+        bbsDao.deleteObjectById(id);
+    }
+
+    @Override
+    public void createBbsPiece(BbsPiece bbsPiece) {
+        bbsPieceDao.saveNewObject(bbsPiece);
+    }
+
+    @Override
+    public void updateBbsPiece(BbsPiece bbsPiece) {
+        bbsPieceDao.updateObject(bbsPiece);
+    }
+
+    @Override
+    public void deleteBbsPieceByID(String id) {
+        bbsPieceDao.deleteObjectById(id);
+    }
+
+
 }
