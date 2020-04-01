6a20723713cce691424dbf471f4ce377d7e00270
xue_qk <xue_qk@centit.com>
2020-04-01 15:43:49 +0800

    对上一次提交进行补充； 1.增加了一些system.properties配置文件 2.增加了测试功能的代码

---
+++ oa-component-module/src/test/java/com/centit/product/oa/TestPiece.java
@@ -0,0 +1,96 @@
+package com.centit.product.oa;
+
+import com.centit.framework.jdbc.config.JdbcConfig;
+import com.centit.product.oa.po.BbsPiece;
+import com.centit.product.oa.service.impl.BbsManagerImpl;
+import com.centit.support.database.utils.PageDesc;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.HashMap;
+import java.util.List;
+
+@PropertySource(value = "classpath:system.properties")
+@ComponentScan(basePackages = {"com.centit"},
+        excludeFilters = @ComponentScan.Filter(value = org.springframework.stereotype.Controller.class))
+@Import(value = {JdbcConfig.class})
+@ContextConfiguration(classes = TestPiece.class)
+@RunWith(SpringRunner.class)
+public class TestPiece {
+
+
+    @Autowired
+    private BbsManagerImpl bbsManager;
+
+
+    @Test
+    public void batchcreateBbsPieceTest(){
+        /*BbsPiece bbsPiece = new BbsPiece();
+        bbsPiece.setApplicationId("asjfioadfa");
+        bbsPiece.setOptId("sfs");
+        bbsPiece.setOptTag("你好");
+        bbsManager.createBbsPiece(bbsPiece);
+        System.out.println("successful");*/
+        for (int i=0;i<10;i++){
+            createBbsPieceTest();
+        }
+    }
+    @Before
+    //@Test
+    public void  createBbsPieceTest(){
+        BbsPiece bbsPiece = new BbsPiece();
+        bbsPiece.setApplicationId("asjfioadfa");
+        bbsPiece.setOptId("sfs");
+        bbsPiece.setOptTag("你好啊");
+        bbsManager.createBbsPiece(bbsPiece);
+        System.out.println("successful");
+    }
+
+    @Test
+    public void updateBbsPieceTest(){
+        BbsPiece bbsPiece = new BbsPiece();
+        bbsPiece.setApplicationId("asjfioadfa");
+        bbsPiece.setOptId("sfs");
+        bbsPiece.setOptTag("你好啊");
+        bbsManager.createBbsPiece(bbsPiece);
+        System.out.println("successful");
+    }
+
+    //listBbsPieces
+    @Test
+    public void listBbsPiecesTest(){
+        HashMap<String, Object> map = new HashMap<>();
+        map.put("applicationId","asjfioadfa");
+        map.put("optId","sfs");
+        List<BbsPiece> bbsPieces = bbsManager.listBbsPieces(map, new PageDesc(1,20));
+        for (BbsPiece bbsPiece : bbsPieces) {
+            System.out.println(bbsPiece.getDeliverDate());
+        }
+        System.out.println("successful");
+    }
+
+    //deleteBbsPieceByID
+    @Test
+    public void deleteBbsPieceByID(){
+
+        bbsManager.deleteBbsPieceByID("iy0R2Hx0TV6bYDDfGaXrDA");
+
+        System.out.println("successful");
+    }
+
+    //根据提供的pieceId获取对应的pieceContent
+    @Test
+    public void getBbsPieceByID(){
+
+        String pieceContent = bbsManager.getBbsPieces("fCFBAFOcSXKuTDlcbC3MCA");
+
+        System.out.println(pieceContent);
+    }
+}
