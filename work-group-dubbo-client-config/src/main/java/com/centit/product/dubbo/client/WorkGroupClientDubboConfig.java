5970a82033eb942ae1a1dbaae144137b3bf56abf
xue_qk <xue_qk@centit.com>
2022-03-31 18:44:57 +0800

    1.删除工作组中相关代码

--- work-group-dubbo-client-config/src/main/java/com/centit/product/dubbo/client/WorkGroupClientDubboConfig.java
+++
@@ -1,9 +0,0 @@
-package com.centit.product.dubbo.client;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-
-@Configuration
-@ImportResource({"classpath:dubbo-workgroup-client.xml"})
-public class WorkGroupClientDubboConfig {
-}
